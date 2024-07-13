8ae207c3bec6b3e07121ff8fdb88346837c4d4fe
DhruvilJariwala <dhruviljariwala987@gmail.com>
2024-07-13 11:46:17 +0530

    Real Estate App

---
+++ client/src/routes/homePage/homePage.jsx
@@ -0,0 +1,47 @@
+import { useContext } from "react";
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./homePage.scss";
+import { AuthContext } from "../../context/AuthContext";
+
+function HomePage() {
+
+  const {currentUser} = useContext(AuthContext)
+
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p>
+          Welcome to DreamHome Finder, where your journey to finding the
+            perfect home begins. Whether you're looking for a cozy apartment in
+            the city, a family house in the suburbs, or a luxurious villa by the
+            beach, we have it all. Our comprehensive platform is designed to
+            help you discover the ideal property that matches your lifestyle and
+            preferences.
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
